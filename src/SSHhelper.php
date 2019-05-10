b83525aa4df13724cb5939eb26d6c32602be8040
assghard <ivan.dublianski@gmail.com>
2019-05-10 10:20:01 +0200

    additional methods, stream

--- src/SSHhelper.php
+++ src/SSHhelper.php
@@ -25,10 +25,16 @@ class SSHhelper {
     }
 
     private function connect() {
+        if(!function_exists('ssh2_connect')) {
+            die('Function function_exists not found. You can not use ssh2 here');
+        }
+
         try {
             if(empty($this->connection)) {
                 $this->connection = ssh2_connect($this->config['host'], $this->config['port']);
                 ssh2_auth_password($this->connection, $this->config['user'], $this->config['password']);
+
+                $this->stream = ssh2_sftp($this->connection);
             }
         } catch (\Exception $exc) {
             echo $exc->getMessage().PHP_EOL;
@@ -44,7 +50,7 @@ class SSHhelper {
     }
 
     /**
-     * Executes command on server via SSH connection
+     * General execution method. Executes command on server via SSH connection
      *
      * @param string $command
      * @return object $stream
@@ -59,4 +65,43 @@ class SSHhelper {
             echo $exc->getMessage();
         }
     }
+
+
+    /*
+     * ========================================================
+     * Additional methods
+     * ========================================================
+     */
+    public function ssh_chmod($folder, $mod = 0755) {
+        try {
+            ssh2_sftp_chmod($this->stream, $folder, $mod);
+        } catch (\Exception $exc) {
+            echo $exc->getMessage();
+        }
+    }
+
+    public function ssh_copy_file($remoteFile, $localFile) {
+        try {
+            copy("ssh2.sftp://{$this->stream}/{$remoteFile}", $localFile);
+        } catch (\Exception $exc) {
+            echo $exc->getMessage();
+        }
+    }
+
+    public function ssh_unlink_file($remoteFile) {
+        try {
+            ssh2_sftp_unlink($this->stream, $remoteFile);
+        } catch (\Exception $exc) {
+            echo $exc->getMessage();
+        }
+    }
+
+    public function ssh_remove_dir($remoteDir) {
+        try {
+            ssh2_sftp_rmdir($this->stream, $remoteDir);
+        } catch (\Exception $exc) {
+            echo $exc->getMessage();
+        }
+    }
+
 }
